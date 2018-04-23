9a26d8257e3a6dc4310f7775c057f2167946175c
ekaterina.dony <ekaterina.dony@yandex.ru>
2018-04-23 14:50:40 +0300

    start

---
+++ script.js
@@ -0,0 +1,30 @@
+var str = '';
+var display = document.querySelector('.result');
+
+var btn = document.querySelectorAll('.btn_act');
+
+for (var i = 0; i < btn.length; i++) {
+	btn[i].addEventListener('click', calc);
+}
+
+var result = document.querySelector('.is');
+result.addEventListener('click', getResult);
+
+var reset = document.getElementById('reset');
+reset.addEventListener('click', clear);
+
+function calc() {
+	str += this.innerHTML;
+	display.innerHTML = str;
+}
+
+function clear() {
+	str = '';
+	display.innerHTML = str;
+}
+
+function getResult() {
+	var answer = eval(str);
+	str = answer;
+	display.innerHTML = answer;
+}
